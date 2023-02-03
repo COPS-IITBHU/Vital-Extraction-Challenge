3ff5d886a4ea69b89b4dbe985fccbd15e5eb04b2
Amshra267 <amanmishra267@gmail.com>
2023-02-04 01:38:23 +0530

    integrated text detector in main and submission.ipynb

--- pipeline/text_detection.py
+++ pipeline/text_detection.py
@@ -1,5 +1,22 @@
+from craft_text_detector import (
+    read_image,
+    load_craftnet_model,
+    load_refinenet_model,
+    get_prediction
+)
 
-
-
-def boundingBoxes(img):
-    return [0.77453733, 0.15082821, 0.07092405, 0.08765801]
+def get_boxes(image):
+  # image = read_image(img_path)
+  refine_net = load_refinenet_model(cuda= False)
+  craft_net = load_craftnet_model(cuda=False)
+  prediction_result = get_prediction(
+      image=image,
+      craft_net=craft_net,
+      refine_net=refine_net,
+      text_threshold=0.7,
+      link_threshold=0.4,
+      low_text=0.4,
+      cuda=True,
+      long_size=1280
+  )
+  return prediction_result['boxes']
